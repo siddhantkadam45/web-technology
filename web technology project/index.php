b3bf1fa64e74b0869ed375532a6b4203ea03359f
siddhantkadam45 <156592199+siddhantkadam45@users.noreply.github.com>
2024-04-18 12:17:04 +0530

    Add files via upload

---
+++ web technology project/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <title>Register</title>
+</head>
+<body>
+<br>
+    <center><span><h1>Register Yourself!!!</h1></span></center> <hr>
+    <div class="container">
+        <br>
+        <br>
+        <div class="card">
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                    <input type="text" class="form-control" id="name" placeholder="Enter your Name">
+            </div>
+
+            <div class="mb-3">
+                <label for="email" class="form-label">Email-id</label>
+                    <input type="email" class="form-control" id="email" placeholder="Enter your Email">
+            </div>
+            
+            <div class="mb-3">
+                <label for="address" class="form-label">Address</label>
+                    <input type="text" class="form-control" id="address" placeholder="Enter your address">
+            </div>
+
+            <div class="mb-3">
+                <label for="address" class="form-label">Phone Number</label>
+                    <input type="text" class="form-control" id="address" placeholder="Enter your address">
+            </div>
+
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" placeholder="Enter Password">
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
